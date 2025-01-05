22465ce8844ec60213631e5ff49d4ff95bf476cb
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-01-05 22:12:33 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -26,7 +26,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('𝚂𝚞𝚙𝚙𝚘𝚛𝚝', url=f'https://t.me/mnbots_support')
+        InlineKeyboardButton('𝚂𝚞𝚙𝚙𝚘𝚛𝚝', url=f'https://t.me/songdownload_group')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
