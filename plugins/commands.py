ea4010230e018c9f0e01e9f2a769e3bde412b9b9
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-01-05 23:32:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,7 +50,7 @@ async def start(client, message):
          ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         #add emoji loading then run 1 sec and dlt
-        m=await message.reply_text("ShobanaFilterBot") 
+        m=await message.reply_text("Starting...") 
         await asyncio.sleep(1.2)#1.2sec sleep
         await m.delete()
         await message.reply_photo(
