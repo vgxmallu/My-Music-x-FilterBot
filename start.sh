7cc7ba1d303c3fbfebd5277f797f3ba3147180a8
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-01-12 20:51:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mn-bots/shobanafilterbot.git /shobanafilterbot
+  git clone https://github.com/Hydra-sjz/My-Music-x-FilterBot.git /My-Music-x-FilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /shobanafilterbot
+  git clone $UPSTREAM_REPO /My-Music-x-FilterBot
 fi
 cd /shobanafilterbot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "🎵 music Filter-𝕏-Bot Starting...."
 python3 bot.py
