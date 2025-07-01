19dca5f540af4f7678581772210b6a1099164441
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-07-01 18:29:38 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -19,13 +19,13 @@ instance = Instance.from_db(db)
 
 @instance.register
 class Media(Document):
-    file_id = fields.StrField(attribute=True) #'_id_'
+    file_id = fields.StrField(attribute='_id_') 
     file_ref = fields.StrField(allow_none=True)
     file_name = fields.StrField(required=True)
     file_size = fields.IntField(required=True)
     file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField() #allow_none=True
-    caption = fields.StrField() #allow_none=True
+    mime_type = fields.StrField(allow_none=True) 
+    caption = fields.StrField(allow_none=True) 
 
     class Meta:
         indexes = ('$file_name', )
